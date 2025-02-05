91c04893972d00d395c6fcacad457a832a826684
Subham531 <dasthemistry@gmail.com>
2025-02-05 23:23:16 +0530

    Update scrape_data.py

--- langchain-chatbot/scrape_data.py
+++ langchain-chatbot/scrape_data.py
@@ -3,7 +3,7 @@ from langchain.chains import conversational_retrieval
 from langchain_community.llms import openai
 import os
 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "api-key"
 
 os.environ["USER_AGENT"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.100 Safari/537.36"
 
