311a6712b396b8cf3a582c2b10e4e2a8286979ce
Subham531 <dasthemistry@gmail.com>
2025-02-05 23:23:43 +0530

    Update create_embeddings.py

--- langchain-chatbot/create_embeddings.py
+++ langchain-chatbot/create_embeddings.py
@@ -4,7 +4,7 @@ from langchain.schema import Document
 import faiss
 import os
 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "api-key"
 
 with open("scraped_data.txt",'r',encoding='utf-8') as f:
     raw_data  = f.readlines()
